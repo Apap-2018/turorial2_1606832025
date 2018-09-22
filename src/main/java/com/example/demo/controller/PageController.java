8bab21cba17d088665bac5f33570ad8d291ed247
rachel.sausan <rachelsausan9g@gmail.com>
2018-09-22 20:17:24 +0700

    menambah jawaban latihan

--- src/main/java/com/example/demo/controller/PageController.java
+++ src/main/java/com/example/demo/controller/PageController.java
@@ -10,26 +10,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PageController {
-	
-	@RequestMapping("/viral")
-	public String index() {
-		return "viral";
-	}
-	@RequestMapping("/challenge")
-	public String challenge(@RequestParam(value = "name") String name, Model model) {
-		model.addAttribute("name", name);
-		return "challenge";
-	}
-	@RequestMapping(value = {"/challenge", "/challenge/{name}"})
-	public String challengePath(@PathVariable Optional<String> name, Model model) {
-		if (name.isPresent()) {
-			model.addAttribute("name", name.get());
-		}else {
-			model.addAttribute("name","KB");
-		}
-		return "challenge";
-	}
-	
+		
+	//Latihan
 	@RequestMapping("/generator")
 	public String generator(@RequestParam(value = "a", required=false, defaultValue="0") int a,
 			@RequestParam(value = "b", required=false, defaultValue="0") int b, Model model) {
@@ -37,18 +19,21 @@ public class PageController {
 		model.addAttribute("a", a);
 		model.addAttribute("b", b);
 		
+		/* hm = var untuk membuat kata hm sesuai banyaknya a
+		 */
 		String hm = "h";
 		
 		if (a>1) {
 			for (int i=0; i<a; i++) {
 				hm += "m";
 			}
-			
 		}else {
 			hm += "m";
-		
 		}
 		
+		/*
+		 * hmm = variabel untuk menyimpan kata hm sebanyak b
+		 */
 		String hmm = "";
 		
 		if(b>1) {
@@ -64,6 +49,37 @@ public class PageController {
 		return "generator";
 	}
 	
-}
 
+
+
+/*
+	@RequestMapping(value = {"/challenge", "/challenge/{name}"})
+	public String challengePath(@PathVariable Optional <String> name, Model model) {
+		if (name.isPresent()) {
+					
+					model.addAttribute("name", name.get());
+				}else {
+					model.addAttribute("name","KB");
+				}
+				return "challenge";
+			
+			
+	}*/
+		
+		
+	
+	@RequestMapping("/viral/challenge")
+	public String challenge(@RequestParam(value = "name") String name, Model model) {
+		model.addAttribute("name", name);
+		return "challenge";
+	}
+	
+	@RequestMapping("/viral")
+	public String index() {
+		return "viral";
+	}
+	
+
+	
+}
 
